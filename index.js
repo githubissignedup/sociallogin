48f9fbe3f90443d3ca869e5903d919c098a6b54c
Nishant Sethi <nishantsethi@Nishants-MacBook-Pro.local>
2022-09-29 10:19:49 +0530

    first api

--- index.js
+++ index.js
@@ -12,4 +12,8 @@ app.post("/signup", async (req, resp) => {
 resp.send(result);
 })
  
-app.listen(5000);
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log("Sever console log.")
+});
+
